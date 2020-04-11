a572c623827b3bf746babc57735f056b398bd76c
Aamir Khan <aamirkhan@beezlabs.com>
2020-04-11 19:14:24 +0530

    commit3

--- services/EventService.js
+++ services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: `https://my-json-server.typicode.com/Code-Pop/real-world-nuxt`,
+  baseURL: `https://gist.githubusercontent.com/aamirkhancr7/528f0b8cfb9fa0f78dc8bac658714647/raw/284ac8c42d0aac754f981d24102689f68f94c9b9/app.json`,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
